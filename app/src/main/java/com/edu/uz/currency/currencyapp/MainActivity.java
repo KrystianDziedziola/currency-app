685c4f92ed23226b92c4372bb5cb5bb2d118c620
kwolkowski <kacper.wolkowski@gmail.com>
2017-03-28 13:48:23 +0200

    CUR-4 Wstepny wyglad MainActivity

--- app/src/main/java/com/edu/uz/currency/currencyapp/MainActivity.java
+++ app/src/main/java/com/edu/uz/currency/currencyapp/MainActivity.java
@@ -1,13 +1,40 @@
 package com.edu.uz.currency.currencyapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.edu.uz.currency.currencyapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.buttonExchangeRates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Przejście do aktywności z Kursami walut.
+            }
+        });
+
+        binding.buttonCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Przejście do aktywności z kalkulatorem.
+            }
+        });
+
+        binding.buttonAtms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Przejście do aktywności z mapa bankomatow.
+            }
+        });
     }
 }
