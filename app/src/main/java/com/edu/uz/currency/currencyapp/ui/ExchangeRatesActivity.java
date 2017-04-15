39fd0d37e4b93d8d05d98ab6cd6119b0cf945fb0
kwolkowski <kacper.wolkowski@gmail.com>
2017-04-15 18:17:32 +0200

    Sprawdzanie internetu i powiadomienie o braku

--- app/src/main/java/com/edu/uz/currency/currencyapp/ui/ExchangeRatesActivity.java
+++ app/src/main/java/com/edu/uz/currency/currencyapp/ui/ExchangeRatesActivity.java
@@ -1,19 +1,24 @@
 package com.edu.uz.currency.currencyapp.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 
 import com.edu.uz.currency.currencyapp.R;
 import com.edu.uz.currency.currencyapp.databinding.ActivityExchangeRatesBinding;
 import com.edu.uz.currency.currencyapp.model.Currency;
-import com.edu.uz.currency.currencyapp.rest.CurrencyService;
 import com.edu.uz.currency.currencyapp.rest.NbpClient;
+import com.edu.uz.currency.currencyapp.service.CurrencyService;
 import com.edu.uz.currency.currencyapp.ui.adapter.CurrenciesAdapter;
 
 import java.io.IOException;
@@ -31,12 +36,34 @@ public class ExchangeRatesActivity extends AppCompatActivity {
 
         binding.recyclerViewCurrencies.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerViewCurrencies.setAdapter(new CurrenciesAdapter(new ArrayList<Currency>(), this));
-        new GetCurrenciesTask().execute();
+
+        if (isOnline()) {
+            new GetCurrenciesTask().execute();
+        } else {
+            // TODO: wczytac dane z bazy
+
+            // TODO: jesli nie ma danych w bazie:
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.internet)
+                    .setMessage(R.string.no_internet_message)
+                    .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            ExchangeRatesActivity.this.finish();
+                        }
+                    }).show();
+        }
     }
 
-    public static void start(Activity activity){
+    public static void start(Activity activity) {
         Intent intent = new Intent(activity, ExchangeRatesActivity.class);
         activity.startActivity(intent);
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 
     class GetCurrenciesTask extends AsyncTask<Void, Void, List<Currency>> {
