7cac6635104851af5fb07aef9a84b1c9ef4f3d91
kwolkowski <kacper.wolkowski@gmail.com>
2017-05-10 16:28:47 +0200

    Pokazywanie dat na wykresie. Zmiana kolorow.

---
+++ app/src/main/java/com/edu/uz/currency/currencyapp/ui/chartElements/MyMarkerView.java
@@ -0,0 +1,21 @@
+package com.edu.uz.currency.currencyapp.ui.chartElements;
+
+import android.content.Context;
+
+import com.github.mikephil.charting.components.MarkerView;
+
+/**
+ * Created by E6420 on 2017-05-10.
+ */
+
+public class MyMarkerView extends MarkerView {
+    /**
+     * Constructor. Sets up the MarkerView with a custom layout resource.
+     *
+     * @param context
+     * @param layoutResource the layout resource to use for the MarkerView
+     */
+    public MyMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+    }
+}
