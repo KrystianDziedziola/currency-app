dc49c30e3bd5a9db4add97b507b481ec45be549b
kwolkowski <kacper.wolkowski@gmail.com>
2017-05-10 16:28:47 +0200

    Pokazywanie dat na wykresie. Zmiana kolorow.

---
+++ app/src/main/java/com/edu/uz/currency/currencyapp/helper/Constants.java
@@ -0,0 +1,9 @@
+package com.edu.uz.currency.currencyapp.helper;
+
+/**
+ * Created by E6420 on 2017-05-10.
+ */
+
+public class Constants {
+    public static final String DATE_FORMAT = "YYYY-MM-dd";
+}
---
+++ app/src/main/java/com/edu/uz/currency/currencyapp/ui/chartElements/ChartXAxisValueFormatter.java
@@ -0,0 +1,26 @@
+package com.edu.uz.currency.currencyapp.ui.chartElements;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import org.joda.time.LocalDate;
+
+import java.util.List;
+
+/**
+ * Created by E6420 on 2017-05-10.
+ */
+
+public class ChartXAxisValueFormatter implements IAxisValueFormatter {
+
+    private List<LocalDate> mDates;
+
+    public ChartXAxisValueFormatter(List<LocalDate> dates) {
+        mDates = dates;
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return mDates.get((int) value).toString("MM-YY");
+    }
+}
